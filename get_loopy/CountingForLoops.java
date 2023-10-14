8b69a1b32ee1a3649363197e2aacc5f758259756
slaibne <ivy.hou330@icloud.com>
2023-10-14 13:38:11 -0400

    decrementing by 2 from 10-0

--- get_loopy/CountingForLoops.java
+++ get_loopy/CountingForLoops.java
@@ -1,10 +1,16 @@
 package get_loopy;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 /**
  * CountingForLoops
  */
 public class CountingForLoops {
     public static void main(String[] args) {
-
+        int i;
+        for (i = 10; i >= 0; i = i - 2) {
+            System.out.println(i);
+        }
     }
+
 }
